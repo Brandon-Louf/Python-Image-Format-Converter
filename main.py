4e5f21d0efcc4a9255692aff6c79c66ddbd26fda
brandon-louf <brandon.louf@gmail.com>
2026-07-31 14:22:45 -0600

    Tested for image format with test image.

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from PIL import Image
 import os
+
+# User input file
+input_file = "image.png"
+
+# Tries to open the input file if it exists as an image file
+try:
+    with Image.open(input_file) as img:
+        input_format = img.format
+        print(f"Input Image Format is {input_format}")
+
+except IOError:
+    print("Error: Invalid image file.")
