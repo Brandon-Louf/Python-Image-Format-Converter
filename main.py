c2b241e10e05d7ccb0d7ec00603075b2281e8a80
brandon-louf <brandon.louf@gmail.com>
2026-08-12 10:29:39 -0600

    Added a tuple of available image formats.

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ input_file = "image.png"
 # Tries to open the input file if it exists as an image file
 try:
     with Image.open(input_file) as img:
+        # The format type of the image (PNG, PDF, ect.)
         input_format = img.format
-        print(f"Input Image Format is {input_format}")
+        print(f"Input image format is {input_format}")
+
+        output_format = ['PNG', 'JPEG', 'PDF', 'WEBP']
+        print(f"What format do you want to convert {input_file} to?")
+
+        # Prints the element in the tuple in order
+        for i, formats, in enumerate(output_format):
+            print(f"{i + 1}. {formats} ")
+
+        selected_format = int(input("Enter a number: "))
 
 except IOError:
     print("Error: Invalid image file.")
