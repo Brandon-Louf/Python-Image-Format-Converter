05c82865a883fc1dbb70360b66cef9450474ec29
brandon-louf <brandon.louf@gmail.com>
2026-08-18 11:02:28 -0600

    A format can now be selected.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 # User input file
 input_file = "image.png"
+selected_format = 0
 
 # Tries to open the input file if it exists as an image file
 try:
@@ -14,11 +15,16 @@ try:
         output_format = ['PNG', 'JPEG', 'PDF', 'WEBP']
         print(f"What format do you want to convert {input_file} to?")
 
-        # Prints the element in the tuple in order
+        # Prints the elements in the tuple in order
         for i, formats, in enumerate(output_format):
             print(f"{i + 1}. {formats} ")
 
         selected_format = int(input("Enter a number: "))
+
+        # Will ask again for a number if the input is invalid
+        while (not 1 <= selected_format <= len(output_format)):
+            selected_format = int(input("Please enter a listed number: "))
+
 
 except IOError:
     print("Error: Invalid image file.")
